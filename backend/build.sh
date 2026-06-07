a49bf65aa77fe3516eca648015f31e7da78a769e
jspcpadilla <padillacampo123@gmail.com>
2026-06-07 14:32:39 -0500

    feat: auto-create superuser on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,18 @@ pip install gunicorn
 pip install -r requirements/base.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+python manage.py shell -c "
+from apps.users.models import User
+import os
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+first_name = os.environ.get('DJANGO_SUPERUSER_FIRSTNAME', '')
+last_name = os.environ.get('DJANGO_SUPERUSER_LASTNAME', '')
+if email and not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password, first_name=first_name, last_name=last_name)
+    print('Superusuario creado exitosamente.')
+else:
+    print('Superusuario ya existe o email no configurado.')
+"
